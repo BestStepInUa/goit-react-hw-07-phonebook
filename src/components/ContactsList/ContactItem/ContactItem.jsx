a246b857d586c4a4422ec9fab899967152b09815
BestStepInUa <beststep.in.ua@gmail.com>
2024-02-10 13:46:54 +0200

    fix v1

--- src/components/ContactsList/ContactItem/ContactItem.jsx
+++ src/components/ContactsList/ContactItem/ContactItem.jsx
@@ -1,15 +1,16 @@
-import { useDispatch } from 'react-redux';
-import { delContact } from '../../../redux/contactsSlice';
+// import { useDispatch } from 'react-redux';
+// import { delContact } from '../../../redux/contactsSlice';
 import ContactItemStyled from './ContactItem.styled';
 
 const ContactItem = ({ contact: { id, name, number } }) => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   return (
     <ContactItemStyled>
       <p>
         {name}: <span>{number}</span>
       </p>
-      <button onClick={() => dispatch(delContact(id))}>Delete</button>
+      <button>Delete</button>
+      {/* <button onClick={() => dispatch(delContact(id))}>Delete</button> */}
     </ContactItemStyled>
   );
 };
